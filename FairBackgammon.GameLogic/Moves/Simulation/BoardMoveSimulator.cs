c0e3d9f9a0f6bbef14ba7c31b75198614198a5a5
AndreiBudaca <marius-andrei.budaca@student.tuiasi.ro>
2026-05-12 17:23:45 +0300

    Checkpoints optimizations

--- FairBackgammon.GameLogic/Moves/Simulation/BoardMoveSimulator.cs
+++ FairBackgammon.GameLogic/Moves/Simulation/BoardMoveSimulator.cs
@@ -7,7 +7,6 @@ namespace FairBackgammon.GameLogic.Moves.Simulation
   public class BoardMoveSimulator(Board board)
   {
     private readonly List<Action> _undoActions = [];
-    private readonly Dictionary<Guid, int> _checkpoints = [];
     private int _movesMade = 0;
 
     public Board Board { get; } = board;
@@ -18,7 +17,7 @@ namespace FairBackgammon.GameLogic.Moves.Simulation
       var toHolder = HolderForIndex(move.to, player);
 
       var (checker, undoRemoveAction) = fromHolder.Remove();
-      _undoActions.Insert(_movesMade, undoRemoveAction);
+      _undoActions.Add(undoRemoveAction);
       ++_movesMade;
 
       if (checker.Type != player)
@@ -27,39 +26,34 @@ namespace FairBackgammon.GameLogic.Moves.Simulation
       }
 
       var (capturedChecker, undoAddAction) = toHolder.Add(checker);
-      _undoActions.Insert(_movesMade, undoAddAction);
+      _undoActions.Add(undoAddAction);
       ++_movesMade;
 
       if (capturedChecker != null)
       {
         var (_, undoBar) = Board.Bar[(int)capturedChecker.Type].Add(capturedChecker);
-        _undoActions.Insert(_movesMade, undoBar);
+        _undoActions.Add(undoBar);
         ++_movesMade;
       }
     }
 
-    public Guid CreateCheckpoint()
-    {
-      var checkpointId = Guid.NewGuid();
-      _checkpoints[checkpointId] = _movesMade;
-      return checkpointId;
-    }
+    public int CreateCheckpoint() => _movesMade;
 
-    public void UndoToCheckpoint(Guid checkpointId)
+    public void UndoToCheckpoint(int checkpointMoveCount)
     {
-      if (!_checkpoints.ContainsKey(checkpointId))
-      {
-        throw new ArgumentException("Invalid checkpoint ID.");
-      }
+      ArgumentOutOfRangeException.ThrowIfNegative(checkpointMoveCount);
+      ArgumentOutOfRangeException.ThrowIfGreaterThan(checkpointMoveCount, _movesMade);
 
-      var targetMoveCount = _checkpoints[checkpointId];
-      for (int i = _movesMade - 1; i >= targetMoveCount; i--)
+      for (int i = _movesMade - 1; i >= checkpointMoveCount; i--)
       {
         _undoActions[i]();
       }
 
-      _movesMade = targetMoveCount;
-      _checkpoints.Remove(checkpointId);
+      _movesMade = checkpointMoveCount;
+      if (_undoActions.Count > checkpointMoveCount)
+      {
+        _undoActions.RemoveRange(checkpointMoveCount, _undoActions.Count - checkpointMoveCount);
+      }
     }
 
     public void UndoAllMoves()
@@ -69,14 +63,12 @@ namespace FairBackgammon.GameLogic.Moves.Simulation
         _undoActions[i]();
       }
 
-      _checkpoints.Clear();
       _undoActions.Clear();
       _movesMade = 0;
     }
 
     public void CommitMoves()
     {
-      _checkpoints.Clear();
       _undoActions.Clear();
       _movesMade = 0;
     }
